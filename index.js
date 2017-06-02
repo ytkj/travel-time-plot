7f767714f2d6bcfd4753e2ee4b7b276ef6a79e23
Yuto Kojima <Kojima@Yuto-no-MacBook-Pro.local>
2017-06-02 23:54:35 +0900

    serialize primises.

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const Scraper = require('./lib/scraper');
 const input = require('./lib/input');
 const output = require('./lib/output');
 
-input.HOME_LIST.forEach(home => {
+let promises = input.HOME_LIST.map(home => {
 
-    Promise.all([
+    return Promise.all([
         api.get(home, input.DESTINATION_X),
         api.get(home, input.DESTINATION_Y)
     ])
     .then(([resX, resY]) => {
         let travelTimeX = (new Scraper(resX.data)).getTravelTime();
         let travelTimeY = (new Scraper(resY.data)).getTravelTime();
-        output.display(home, travelTimeX, travelTimeY);
+//        output.display(home, travelTimeX, travelTimeY);
+        output.plot(home, travelTimeX, travelTimeY);
     });
 
 });
+
+promises.reduce((prev, curr) => {
+    return prev.then(curr);
+}, Promise.resolve());
